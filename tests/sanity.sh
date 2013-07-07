10606b25a92156f415ce70cca929b59bdfbe41e8
KO Myung-Hun <komh@chollian.net>
2013-07-07 23:45:19 +0900

    Initial import of gtk-doc v1.19

---
+++ tests/sanity.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+dir=`dirname $0`
+suite="sanity"
+
+failed=0
+tested=0
+
+echo "Running suite(s): gtk-doc-$suite";
+
+# check the presence and non-emptyness of certain files
+nok=0
+for path in $dir/*/docs*/html; do
+  if test ! -s $path/index.html ; then
+    echo 1>&2 "no or empty $path/index.html"
+    nok=`expr $nok + 1`; break;
+  fi
+  if test ! -s $path/index.sgml ; then
+    echo 1>&2 "no or empty $path/index.sgml"
+    nok=`expr $nok + 1`; break;
+  fi
+  if test ! -s $path/home.png ; then
+    echo 1>&2 "no or empty $path/home.png"
+    nok=`expr $nok + 1`; break;
+  fi
+  file=`echo $path/*.devhelp2`
+  if test ! -s $file ; then
+    echo 1>&2 "no or empty $file"
+    nok=`expr $nok + 1`; break;
+  fi
+done
+if test $nok -gt 0 ; then failed=`expr $failed + 1`; fi
+tested=`expr $tested + 1`
+
+
+# check online/anchor tags
+nok=0
+for file in $dir/*/docs*/html/index.sgml; do
+  grep >/dev/null "<ONLINE href=" $file
+  if test $? = 1 ; then
+    echo 1>&2 "missing ONLINE reference in $file"
+    nok=`expr $nok + 1`; break;
+  fi
+  grep >/dev/null "<ANCHOR id=" $file
+  if test $? = 1 ; then
+    echo 1>&2 "missing ANCHOR reference in $file"
+    nok=`expr $nok + 1`; break;
+  fi
+done
+if test $nok -gt 0 ; then failed=`expr $failed + 1`; fi
+tested=`expr $tested + 1`
+
+
+# check validity of generated xml files
+nok=0
+for file in $dir/*/docs*/xml/*.xml; do
+  xmllint --noout --noent $file
+  if test $? != 0 ; then
+    echo 1>&2 "xml validity check failed for $file"
+    nok=`expr $nok + 1`;
+  fi
+done
+if test $nok -gt 0 ; then failed=`expr $failed + 1`; fi
+tested=`expr $tested + 1`
+
+
+# check validity of generated sgml files
+nok=0
+for file in $dir/*/docs*/xml/*.sgml; do
+  xmllint --noout --noent $file
+  if test $? != 0 ; then
+    echo 1>&2 "sgml validity check failed for $file"
+    nok=`expr $nok + 1`;
+  fi
+done
+if test $nok -gt 0 ; then failed=`expr $failed + 1`; fi
+tested=`expr $tested + 1`
+
+
+# summary
+successes=`expr $tested - $failed`
+rate=`expr 100 \* $successes / $tested`;
+echo "$rate %: Checks $tested, Failures: $failed"
+
+test $failed = 0
+exit $?
---
+++ tests/tools.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+suite="tools"
+
+failed=0
+tested=0
+
+echo "Running suite(s): gtk-doc-$suite";
+
+# we can use which here as we override the path in TEST_ENVIRONMENT
+
+# test perl scripts
+for file in gtkdoc-check gtkdoc-fixxref gtkdoc-mkdb gtkdoc-mktmpl gtkdoc-rebase gtkdoc-scan gtkdoc-scangobj gtkdoc-scanobj ; do
+  /usr/bin/perl -cwT `which $file`
+  if test $? = 1 ; then failed=`expr $failed + 1`; fi
+  tested=`expr $tested + 1`
+done
+
+
+# test shell scripts
+for file in gtkdoc-mkhtml gtkdoc-mkman gtkdoc-mkpdf gtkdocize; do
+  sh -n `which $file`
+  if test $? != 0 ; then failed=`expr $failed + 1`; fi
+  tested=`expr $tested + 1`
+done
+
+
+# test xsl files
+for file in $ABS_TOP_SRCDIR/*.xsl; do
+  xmllint --noout --noent $file
+  if test $? != 0 ; then failed=`expr $failed + 1`; fi
+  tested=`expr $tested + 1`
+done
+
+# summary
+successes=`expr $tested - $failed`
+
+rate=`expr 100 \* $successes / $tested`;
+echo "$rate %: Checks $tested, Failures: $failed"
+
+test $failed = 0
+exit $?
